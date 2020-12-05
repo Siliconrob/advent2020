a209159fe5a856017ca9f7e78a2bcfc8ddfd7c64
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2020-12-05 13:46:52 -0600

    Update day3.py
    Now with part 2

--- day3.py
+++ day3.py
@@ -1,4 +1,14 @@
 from aocd import get_data
+import math
+
+class Slope:
+    def __init__(self, left, down):
+        self.down = down
+        self.left = left
+    trees = 0
+    down = 1
+    left = 1
+
 
 if __name__ == '__main__':
     data = get_data(day=3)
@@ -21,3 +31,19 @@ if __name__ == '__main__':
         x += left
 
     print(f'Trees Part 1: {trees}')
+
+    slopes = [Slope(1, 1), Slope(3, 1), Slope(5, 1), Slope(7, 1), Slope(1, 2)]
+    
+    for slope in slopes:
+        x = 0
+        for i in range(0, len(data_lines), slope.down):
+            current_line = data_lines[i]
+            line_length = len(current_line)
+            next_position = x % line_length
+            char_to_check = current_line[next_position]
+            if char_to_check == '#':
+                slope.trees += 1
+            x += slope.left
+        print(f'Trees Slope ({slope.left}, {slope.down}): {slope.trees}')
+
+    print(f'Trees Part 2: {math.prod([slope.trees for slope in slopes])}')
