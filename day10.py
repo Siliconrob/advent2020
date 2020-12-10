f7962937fc07caabe143ac83ff8598d31cd5d2b2
Nibbles <siliconrob@siliconheaven.net>
2020-12-09 21:22:31 -0600

    Create day10.py

---
+++ day10.py
@@ -0,0 +1,29 @@
+from aocd import get_data
+from parse import parse
+
+if __name__ == '__main__':
+    # data = [
+    #     35,
+    #     20,
+    #     15,
+    #     25,
+    #     47,
+    #     40,
+    #     62,
+    #     55,
+    #     65,
+    #     95,
+    #     102,
+    #     117,
+    #     150,
+    #     182,
+    #     127,
+    #     219,
+    #     299,
+    #     277,
+    #     309,
+    #     576
+    # ]
+
+    data = get_data(day=10)
+    print(data)
