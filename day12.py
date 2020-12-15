86c1130279321979f3d45979804a328841ba5aa3
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2020-12-14 20:47:45 -0600

    Attempt Part 1

--- day12.py
+++ day12.py
@@ -1,25 +1,46 @@
+from aocd import get_data
 from parse import parse
 
 if __name__ == '__main__':
-    # data = get_data(day=12).splitlines()
+    data = get_data(day=12).splitlines()
 
-    data = [
-        "F10",
-        "N3",
-        "F7",
-        "R90",
-        "F11"
-    ]
+    # data = [
+    #     "F10",
+    #     "N3",
+    #     "F7",
+    #     "R90",
+    #     "F11"
+    # ]
 
-    start_orientation = "E"
+    current_direction = 90
+    x, y = 0, 0
 
-    manhattan_distance = 0
     for data_line in data:
         action, move = parse("{}{:d}", data_line)
         print(f'{action} {move}')
+        if action == "F":
+            if current_direction == 90:
+                x += move
+            if current_direction == 180:
+                y -= move
+            if current_direction == 0:
+                y += move
+            if current_direction == 270:
+                x -= move
+        if action == "R":
+            current_direction = abs((current_direction + move) % 360)
+        if action == "L":
+            current_direction = abs((current_direction - move) % 360)
+        if action == "N":
+            y += move
+        if action == "S":
+            y -= move
+        if action == "E":
+            x += move
+        if action == "W":
+            x -= move
 
-
-
+    manhattan_distance = abs(x) + abs(y)
     print(f'Part 1: Manhattan distance {manhattan_distance}')
 
 
