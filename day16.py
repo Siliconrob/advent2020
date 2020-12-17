72155a63d34a1aef3b0247e5ddd9d812cb0ca35a
Nibbles <siliconrob@siliconheaven.net>
2020-12-16 21:17:04 -0600

    FIxed for part 1 passing

--- day16.py
+++ day16.py
@@ -1,4 +1,4 @@
-# from aocd import get_data
+from aocd import get_data
 import sqlite3
 from parse import parse
 
@@ -15,19 +15,19 @@ class Ticket:
 
 def parse_rules(input_rules):
     rules = []
-    for input_rule in input_rules:
+    for input_rule in input_rules.split("\n"):
         name, min_rule1, max_rule1, min_rule2, max_rule2 = parse("{}: {:d}-{:d} or {:d}-{:d}", input_rule)
         rules.append(InputRule(name, min_rule1, max_rule1))
         rules.append(InputRule(name, min_rule2, max_rule2))
     return rules
 
 def parse_my_ticket(my_ticket_input):
-    ids = [int(number) for number in my_ticket_input[-1].split(',')]
+    ids = [int(number) for number in my_ticket_input.split("\n")[-1].split(',')]
     return Ticket("your ticket", ids)
 
 def parse_nearby_tickets(nearby_ticket_inputs):
     nearby_tickets = []
-    for ticket_data in nearby_ticket_inputs:
+    for ticket_data in nearby_ticket_inputs.split("\n"):
         if ticket_data.startswith("nearby tickets:"):
             continue
         ids = [int(number) for number in ticket_data.split(',')]
@@ -36,26 +36,26 @@ def parse_nearby_tickets(nearby_ticket_inputs):
 
 
 if __name__ == '__main__':
-    data = [
-        [
-            "class: 1-3 or 5-7",
-            "row: 6-11 or 33-44",
-            "seat: 13-40 or 45-50"
-        ],
-        [
-            "your ticket:",
-            "7, 1, 14"
-        ],
-        [
-            "nearby tickets:",
-            "7, 3, 47",
-            "40, 4, 50",
-            "55, 2, 20",
-            "38, 6, 12"
-        ]
-    ]
+    # data = [
+    #     [
+    #         "class: 1-3 or 5-7",
+    #         "row: 6-11 or 33-44",
+    #         "seat: 13-40 or 45-50"
+    #     ],
+    #     [
+    #         "your ticket:",
+    #         "7, 1, 14"
+    #     ],
+    #     [
+    #         "nearby tickets:",
+    #         "7, 3, 47",
+    #         "40, 4, 50",
+    #         "55, 2, 20",
+    #         "38, 6, 12"
+    #     ]
+    # ]
 
-    # data = get_data(day=16).split("\n\n")
+    data = get_data(day=16).split("\n\n")
 
     input_rules = parse_rules(data[0])
     conn = sqlite3.connect(":memory:")
