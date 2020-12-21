e19b0c4b83a351bdcdc1a14641db61c7702bb69f
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2020-12-20 18:54:24 -0600

    Create day17.py

---
+++ day17.py
@@ -0,0 +1,13 @@
+data = """##..#.#.
+        ###.#.##\n
+        ..###..#\n
+        .#....##\n
+        .#..####\n
+        #####...\n
+        #######.\n
+        #.##.#.#"""
+
+if __name__ == '__main__':
+    # This problem has been solved by others using convolve in numpy but I don't know how that works now
+    answer = "??"
+    print(f'Part 1: {answer}')
